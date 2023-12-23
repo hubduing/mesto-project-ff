7206a99a5379fe4f76139df09fa39c7539078998
Alexey Borisov <UralMana@yandex.ru>
2023-12-23 11:47:02 +0500

    Добавление и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const placeList = document.querySelector('.places__list');
+const template = document.querySelector('#card-template').content;
+
+
+// @todo: Темплейт карточки
+function templateFunction(card) {
+  const container = template.querySelector('.places__item');
+  const cardTemplate = container.cloneNode(true);
+  const buttonRemove = cardTemplate.querySelector('.card__delete-button');
+  
+  cardTemplate.querySelector('.card__title').textContent = card.name;
+  cardTemplate.querySelector('.card__image').src = card.link;
+   
+  placeList.append(cardTemplate);
+
+  buttonRemove.addEventListener('click', (event) => {
+    return removeCard(event);
+  });
+}
 
 // @todo: Функция создания карточки
-
+function addCard() {
+  
+}
 // @todo: Функция удаления карточки
-
+function removeCard(event) {
+  event.target.parentNode.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  templateFunction(card);
+});
