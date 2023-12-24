1aa8e8ca75de9e7113458a450e83f69cbeac60f0
Alexey Borisov <UralMana@yandex.ru>
2023-12-24 08:59:09 +0500

    Перенаименование констант. Удаление логики через event.

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,29 @@
 // @todo: DOM узлы
-const placeList = document.querySelector('.places__list');
-const template = document.querySelector('#card-template').content;
-
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Темплейт карточки
-function templateFunction(card) {
-  const container = template.querySelector('.places__item');
-  const cardTemplate = container.cloneNode(true);
-  const buttonRemove = cardTemplate.querySelector('.card__delete-button');
+function createCard(cardArray, removeCard) {
+  const container = cardTemplate.querySelector('.places__item');
+  const card = container.cloneNode(true);
+  const buttonRemove = card.querySelector('.card__delete-button');
   
-  cardTemplate.querySelector('.card__title').textContent = card.name;
-  cardTemplate.querySelector('.card__image').src = card.link;
-   
-  placeList.append(cardTemplate);
+  card.querySelector('.card__title').textContent = cardArray.name;
+  card.querySelector('.card__image').src = cardArray.link;
+  card.querySelector('.card__image').alt = cardArray.alt;
 
-  buttonRemove.addEventListener('click', (event) => {
-    return removeCard(event);
-  });
+  buttonRemove.addEventListener('click', () => removeCard(buttonRemove.closest('.card')));
+
+  return card;
 }
 
-// @todo: Функция создания карточки
-function addCard() {
-  
-}
 // @todo: Функция удаления карточки
-function removeCard(event) {
-  event.target.parentNode.remove();
+function removeCard(card) {
+  card.remove();
 }
+
 // @todo: Вывести карточки на страницу
-initialCards.forEach((card) => {
-  templateFunction(card);
+initialCards.forEach((cardArray) => {
+  let cardTemplate = createCard(cardArray, removeCard);
+  cardsContainer.append(cardTemplate);
 });
