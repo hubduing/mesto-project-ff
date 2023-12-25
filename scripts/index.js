8eaac7fece1dc7ac726e56f68b08fb89dae7e4a7
Alexey Borisov <UralMana@yandex.ru>
2023-12-25 15:59:47 +0500

    Исправление в нейминге.

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,18 @@ const cardsContainer = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Темплейт карточки
-function createCard(cardArray, removeCard) {
+function createCard(cardData, removeCard) {
   const container = cardTemplate.querySelector('.places__item');
   const card = container.cloneNode(true);
   const buttonRemove = card.querySelector('.card__delete-button');
+  const cardImage = card.querySelector('.card__image');
   
-  card.querySelector('.card__title').textContent = cardArray.name;
-  card.querySelector('.card__image').src = cardArray.link;
-  card.querySelector('.card__image').alt = cardArray.alt;
+  card.querySelector('.card__title').textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.alt;
 
-  buttonRemove.addEventListener('click', () => removeCard(buttonRemove.closest('.card')));
-
+  buttonRemove.addEventListener('click', () => removeCard(card));
+  
   return card;
 }
 
@@ -23,7 +24,7 @@ function removeCard(card) {
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach((cardArray) => {
-  let cardTemplate = createCard(cardArray, removeCard);
+initialCards.forEach((cardData) => {
+  let cardTemplate = createCard(cardData, removeCard);
   cardsContainer.append(cardTemplate);
 });
